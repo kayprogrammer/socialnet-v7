0be2a5d0dcd09f054dd88c01c2c7e477cbbafcf2
kene <kenechiifeanyi@gmail.com>
2024-10-22 21:26:44 +0100

    updated buildfiles

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
 npm install
 npx ts-node seeding/data.ts  # Use npx to run ts-node from local node_modules
-npx tsc  # Use npx to run the TypeScript compiler
+npx tsc && cp -R templates dist/templates # Use npx to run the TypeScript compiler
 echo "BUILD END"
